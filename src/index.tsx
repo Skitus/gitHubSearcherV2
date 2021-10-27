668faa1088731a9e74c00c76a4684caaaf211046
Skitus <artur.demenskiy03@gmail.com>
2021-10-27 19:14:30 +0300

    fix bug with esLint

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from './store/strore';
 import App from './App';
 import './index.scss';
