3d484cf8d0c9cc30f51631e78d531330f0c67bfe
Skitus <80597741+Skitus@users.noreply.github.com>
2021-12-11 19:59:56 +0200

    Develop (#2)
    * clear all
    
    * create app
    
    (cherry picked from commit 3fad17799a4bbeeb5bf9edf1ae2b5f0ae19fdde7)
    
    * add tsconfig
    
    (cherry picked from commit b1e646bbfe60c0a6272709206fae75c1d7296c5f)
    
    * dev creaete pR
    
    * clear layout
    
    * add selectors & add new components
    
    * add eslint
    
    * clear layout
    
    * fix PR changes
    
    * fix PR changes
    
    * fix responsive layout
    
    * fix responsive layout
    
    * fix layout
    
    * formating
    
    * fix bug with esLint
    
    * update v node
    
    * fix esLint
    
    * add pagination
    
    * fix layout
    
    * add pagination to user
    
    * add infiniti
    
    * fix bug with infinity add some styles
    
    * complete fix bug with infiniti
    
    * fix bug with query
    
    * fix bug with loader
    
    * complete fix bug with query
    
    * complete fix bug with scroll
    
    * fix deploy
    
    * add skeleton
    
    * fix type
    
    * fixx deploy
    
    * fix pagination
    
    * delete console.log()
    
    * fix styles pos fixed
    
    * fix PR
    
    * fix PR
    
    * fix PR
    
    * fix PR
    
    * change token

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { HomeRoute } from './routes/Routes';
 import './App.scss';
-
+// change git hub  token
 function App() {
   return (
     <div className="container">
